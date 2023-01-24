d112f3419afd950f9a8ae9e2647ac27397eaaa39
sest0003 <123386738+sest0003@users.noreply.github.com>
2023-01-24 13:54:16 +0100

    Create Hej.java

---
+++ Hej.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("This is the Git checkpoint task 1.1");
+
+        Calculate c = new Calculate ("bob");
+
+    }
+}
